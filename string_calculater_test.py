0e0a1f4e702db982a9d5cf138f9361d53bbb8816
RSili-Tech <88551701+RSili-Tech@users.noreply.github.com>
2022-05-29 21:25:46 +0530

    cistome delimeters

--- string_calculater_test.py
+++ string_calculater_test.py
@@ -36,3 +36,8 @@ def test_stringcalculater_to_add_for_newlines_between_number():
 def test_stringcalculater_to_show_negative_numbers_not_allowed():    
     input = '-11, -2, -14, 1, 5, 3'   
     Add(input)
+
+def test_stringcalculater_to_add_when_string_has_custome_delimeters():    
+    input = '//;\n1;2"'   
+    Add(input)
+
